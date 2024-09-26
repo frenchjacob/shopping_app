0e14c748ac9bd2f0f5514a3709c909e47fa8d0e0
Jacob French <jacob.french@spark.co.nz>
2024-09-27 11:44:50 +1200

    returning style html

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
-const http = require('http');
+const path = require('path');
+const express = require("express");
+const bodyParser = require('body-parser');
 
-const routes = require('./routes');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-const server = http.createServer(routes);
 
-server.listen(3000);
+const app = express();
+
+// Parse incoming request text
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/admin',adminRoutes);
+
+app.use(shopRoutes);
+
+app.use((req,res,next)=>{
+    res.status(404).sendFile(path.join(__dirname,'views', '404.html'));
+});
+
+app.listen(3000);
