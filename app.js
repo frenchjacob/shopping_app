89eaf6ae1aee9c641711f7374e8f249e9c40b659
Jacob French <jacob.french@spark.co.nz>
2024-09-27 19:38:35 +1200

    MVC model development

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
-const path = require('path');
+const path = require("path");
 const express = require("express");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
+const errorController = require("./controllers/error")
 
 const app = express();
 
-// Parse incoming request text
-app.use(bodyParser.urlencoded({extended: false}));
+app.set("view engine", "pug");
 
-app.use('/admin',adminRoutes);
+// Parse incoming request text
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminRoutes);
 
 app.use(shopRoutes);
 
-app.use((req,res,next)=>{
-    res.status(404).sendFile(path.join(__dirname,'views', '404.html'));
-});
+app.use(errorController.get404);
 
 app.listen(3000);
