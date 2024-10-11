bd246c42f5cf76ed6308c3b8d422fb87f8109d45
Jacob French <jacob.french@spark.co.nz>
2024-10-11 14:52:41 +1300

    created mysql connection

--- app.js
+++ app.js
@@ -5,9 +5,19 @@ const bodyParser = require("body-parser");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
-const errorController = require("./controllers/error")
+const errorController = require("./controllers/error");
+
+const db = require("./util/database");
 
 const app = express();
+
+db.execute("SELECT * FROM PRODUCTS")
+  .then(results => {
+    console.log(results)
+  })
+  .catch(err => {
+    console.log(err)
+  });
 
 app.set("view engine", "ejs");
 
